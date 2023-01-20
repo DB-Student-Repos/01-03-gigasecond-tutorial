bac35455d449bb0084893b5b84aa24fc6e658e0d
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2023-01-20 22:48:04 +0100

    Update gigasecond.py

--- gigasecond.py
+++ gigasecond.py
@@ -1,5 +1,11 @@
-from datetime import datetime, timedelta
+#from datetime import datetime, timedelta
 
-def add(moment):
-    return(moment + timedelta(seconds = 1e9))
+#def add(moment):
+#   return(moment + timedelta(seconds = 1e9))
 
+
+import datetime
+def add (moment):
+    time_change = datetime.timedelta(seconds=1e9)
+    new_time = moment + time_change
+    return new_time
